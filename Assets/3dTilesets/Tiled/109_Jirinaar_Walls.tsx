1403367a59556d441eb7676497c2e31679ee0328
Cam Sinclair <csinkers@gmail.com>
2022-02-13 02:35:24 -0500

    Test map updates

---
+++ Assets/3dTilesets/Tiled/109_Jirinaar_Walls.tsx
@@ -0,0 +1,1 @@
+<?xml version="1.0" encoding="utf-8"?><tileset name="Labyrinth.Jirinaar.Walls" tilewidth="96" tileheight="128" columns="0" tilecount="1" version="1.4" tiledversion="1.4.2" backgroundcolor="#000000"><grid orientation="isometric" width="96" height="128" /><image source="Gfx/109_Jirinaar_Walls.png" width="1536" height="128" /><tile id="0" /></tileset>
